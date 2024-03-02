98ed95014824032a2e40e35cb12982de8c87f389
Vinod Desai <41861786+vinod-desai@users.noreply.github.com>
2024-03-02 11:40:01 +0530

    Create Todo.java

---
+++ src/main/java/com/example/couchbasedemo/model/Todo.java
@@ -0,0 +1,48 @@
+package com.example.couchbasedemo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.couchbase.core.mapping.Document;
+import org.springframework.data.couchbase.core.mapping.Field;
+import org.springframework.data.couchbase.repository.Collection;
+import org.springframework.data.couchbase.repository.Scope;
+
+@Document
+@Scope("TodoApp")
+@Collection("Todo")
+public class Todo {
+    @Id
+    private String id;
+    @Field
+    private String name;
+    @Field
+    private boolean done;
+
+    public Todo() {
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+}
